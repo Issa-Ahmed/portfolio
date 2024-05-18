09e69711a191f6cdbddb7e366efc63c6830e5d5f
Issa Ahmed <mrissamohamud@gmail.com>
2024-05-18 22:08:28 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./resources/**/*.{html,js}"],
+  content: [
+    "./resources/**/*.{html,js}",
+    "./**/*.html"
+  ],
   theme: {
     extend: {
       colors: {
